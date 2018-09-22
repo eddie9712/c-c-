3399ad32e5b8573ebd9deff75c412e6d38f9bd9e
eddie <qw1562435@gmail.com>
2018-09-22 18:41:12 +0800

    new

---
+++ hw3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlibi>
+#define max 100000
+void reverse(char *,int);
+ int main()
+{
+   int number,i;
+   scanf("%d",&number);
+   for(i=0;i<number;i++)
+  { 
+    char *array=calloc(max,sizeof(char));
+
+  }
+
+}
+ void reverse()
+{
+
+
+
+
+
+}
