91c6ded925edd1e03d972cb6d5331a14ff690d2f
unknown <qw1562435@gmail.com>
2019-02-22 10:55:19 +0800

    new

--- datastructureC/multipoly.c
+++ datastructureC/multipoly.c
@@ -1,3 +1,54 @@
 #include<stdio.h>
 #include<stdlib.h>
+#define max1 100
+#define max2 10000
+typedef struct 
+{
+    int coef;
+    int expon;
+}polynomial;
+void multipoly();
+int main()
+{
+    int first,second,i,j;
+    polynomial terms1[max];
+    polynomial terms2[max];
+    polynomial  terms3[max];
+    printf("enter the terms of first polynomial:");  //input the polynomial
+    scanf("%d",&first);
+    printf("enter the first polynomial:");
+    for(i=0;i<first;i++)
+    {
+      printf("coefficient:");
+      scanf("%d",terms1[i].coef);
+      printf("exponent:");
+      scanf("%d",terms1[i].expon);
+    }
+    printf("enter the terms of second polynomial:");
+    scanf("%d",&second);
+    printf("enter the first polynomial:");
+    for(j=0;j<second;j++)
+    {
+       printf("coefficient:");
+      scanf("%d",terms2[i].coef);
+      printf("exponent:");
+      scanf("%d",terms2[i].expon);
+    }
+    multipoly(terms1,terms2,terms3,first,second);
+
+}
+void multipoly(polynomial terms1[],polynomial terms2[],polynomial terms3[],int a,int b)
+{
+  int i,j,expon3,coef3;
+  for(i=0;i<a;i++)
+  {
+    for(j=0;j<b;j++)
+    {
+      expon3=terms1[i].expon+terms2[i].expon;
+      coef3=terms1[i].coef*terms2[i].coef;
+      terms3[expon3].coef+=coef3;  //same coef accumulate  expon for inde
+      
+    }
+  }
+}
 
--- datastructureC/readandbuildv2.c
+++ datastructureC/readandbuildv2.c
@@ -7,27 +7,34 @@ typedef struct
   int expon;
 
 }polynomial
-polynomial terms[max];
 int main()
 {
-  readpoly();
-  printpoly();
+  int n;
+  poliynomial terms[max];
+  printf("enter the terms of the polynomial:");
+  scanf("%d",&n);
+  readpoly(terms,n);
+  printpoly(terms,n);
 
 }
 void printpoly(polynomial terms [],int n)
 {
  int i;
  for(i=0;i<n-1;i++)
-  printf("%dx^%d\n")
-
-
-
-
+  printf("%dx^%d+",terms[i].coef,terms[i].expon);
+ printf("%dx^%d\n",terms[n-1].coef,terms[i].expon);
 }
-void readpoly()
+void readpoly(polynomial terms[],int n)
 {
-
-
-
-
+  int i,expon,coef;
+  if(n>max){
+    printf("too many terms in your polymial!");
+  }
+  for(i=0;i<n;i++){
+   printf("coefficient: ");
+   scanf("%d",&coef);
+   printf("exponent: ");
+   terms[i].coef=coef;
+   terms[i].expon=expon;
+  }
 }
