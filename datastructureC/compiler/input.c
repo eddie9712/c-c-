2c8a84d4f1ae0fae3cea63fdf7f592991f070dcd
eddie9712 <qw1562435@gmail.com>
2019-04-03 23:49:35 +0800

    work of compiler

---
+++ datastructureC/compiler/input.c
@@ -0,0 +1,31 @@
+/*
+ *spring Compiler Course Assignment 1
+ */
+
+int main() {
+    /// Declaration
+    int x;
+    int a = 5;
+    string y = "Hello World";
+    [dfe];
+    print(y);
+    5>=8;
+    4==0
+    5.5==
+    8+=
+    5*=
+   vneo||wegwe
+    foegi&&ewkgnl
+    5/=
+    6%=
+    [ewf&&ewfew];
+    5++;
+    6--;
+    A+b=7.657;
+    // if condition
+    if (a > 10) {
+        x += a;
+        print(x);
+    }
+    return 0;
+}
