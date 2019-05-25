5475d52052f634b148e93d5a72d012f53dc9b877
unknown <qw1562435@gmail.com>
2019-05-25 15:15:29 +0800

    radixsort practice

--- datastructureC/sort/radixsort.c
+++ datastructureC/sort/radixsort.c
@@ -1,12 +1,62 @@
-#include<stdio.h>
+#include<stdio.h>  //radix sort for digit d(using array)
+#include<math.h>
 #include<stdlib.h>
-int main();
+void radixsort(int *,int,int);
+void insertionsort(int *,int *,int);
+int main()
 {
-    
-  //radix(A,d)
-  //for i to d digits
-  //use an sort for each digit
-  //if use count sort =>(not in place) for more place
-  //if use insertion sort =>(inplace) however run time nor for O(n)
-
+  int d,i,n;
+  printf("input the digit of d");
+  scanf("%d",&d);
+  printf("input the  numbers of interger:");
+  scanf("%d",&n);
+  int *array=calloc(n,sizeof(int));
+   printf("input the  numbers of radix sort:");
+  for(i=0;i<n;i++)
+  {
+   scanf("%d",&array[i]);
+  }
+  radixsort(array,d,n);
+  printf("output:\n");
+  for(i=0;i<n;i++)
+  {
+    printf("%d\n",array[i]);         
+  }  
+}
+void radixsort(int array[],int d,int n)
+{
+  int k=1;
+  int j,i;
+  int *lsd=calloc(n,sizeof(int)) ;
+  for(i=1;i<=d;i++)
+  {
+    for(j=0;j<n;j++)
+    {
+      lsd[j]=(array[j]/k)%10;
+    }
+    insertionsort(array,lsd,n);
+    k*=10;
+  }
+}
+void insertionsort(int array[],int lsd[],int n)  //using a stable sort for radix sort
+{
+  int i,j,key;
+  int temp;
+  for(j=1;j<n;j++)
+  {
+    i=j-1;
+    key=lsd[j];
+    temp=array[j];
+    while(i>=0&&lsd[i]>key) 
+    {
+      array[i+1]=array[i];
+      lsd[i+1]=lsd[i];
+      i--;
+    }
+    array[i+1]=temp;
+    lsd[i+1]=key;
+  }
+ // printf("@@@@@\n");
+ // for(i=0;i<n;i++)
+ //  printf("%d\n",lsd[i]); 
 }
